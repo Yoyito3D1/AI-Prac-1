028042371e3cd8d4018c76d25be1a4dcd5a971e4
Jon Jordi Salvadó Pérez <jonjordi.salvado@estudiants.urv.cat>
2025-03-01 12:19:08 +0100

    Esquelet inicial del codi

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            MapGrid map = new MapGrid("src/map.txt");
+            map.printMap();
+        } catch (IOException e) {
+            System.err.println("Error carregant el mapa: " + e.getMessage());
+        }
     }
 }
---
+++ src/Cell.java
@@ -0,0 +1,12 @@
+public class Cell {
+    int x, y;
+    int height;
+    boolean isCliff; // Indica si és un precipici
+
+    public Cell(int x, int y, int height, boolean isCliff) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.isCliff = isCliff;
+    }
+}
